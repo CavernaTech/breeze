f542b0e65ccc42720b1670117cbb55833ec2d7b2
José Gabriel Gruber <jgabrielgruber@hotmail.com>
2022-03-09 15:02:17 -0300

    Implementado checkout;

---
+++ src/components/checkout/style.js
@@ -0,0 +1,49 @@
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles((theme) => ({
+  checkout: {
+    width: "100vw",
+    height: "80vh",
+    left: 0,
+    bottom: 0,
+    top: "auto",
+    whiteSpace: "nowrap",
+    position: "fixed",
+  },
+  checkoutOpen: {
+    transition: theme.transitions.create("height", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  checkoutClose: {
+    transition: theme.transitions.create("height", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    height: "10vh",
+  },
+  fab: {
+    position: "fixed",
+    right: "7vh",
+    bottom: "7vh",
+  },
+  puller: {
+    padding: 20,
+    paddingTop: 8,
+    borderRadius: 3,
+    position: "absolute",
+    top: 0,
+    left: "calc(50% - 15px)",
+  },
+  greyBox: {
+    width: 30,
+    height: 6,
+    backgroundColor:
+      theme.palette.mode === "light"
+        ? theme.palette.grey[300]
+        : theme.palette.grey[900],
+  },
+}));
+
+export default useStyles;
--- src/pages/terminal.page.jsx
+++ src/pages/terminal.page.jsx
@@ -1,19 +1,32 @@
+import {
+  AppBar,
+  MenuItem,
+  Select,
+  Toolbar,
+} from "@mui/material";
+
 import MenuContainer from "../containers/menu.container";
+import CheckoutComponent from "../components/checkout";
 
 function TerminalPage({ isAuthenticated }) {
-  if (!isAuthenticated) {
-    return (
-      <p>aaa</p>
-    );
-  }
   return (
     <MenuContainer>
+      <AppBar position="fixed">
+        <Toolbar>
+          <Select displayEmpty fullWidth value="1">
+            <MenuItem disabled value="1">
+              Mesa 1
+            </MenuItem>
+          </Select>
+        </Toolbar>
+      </AppBar>
+      <CheckoutComponent />
     </MenuContainer>
   );
 }
 
 TerminalPage.defaultProps = {
   isAuthenticated: false,
-}
+};
 
 export default TerminalPage;
